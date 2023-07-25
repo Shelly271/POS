41214719e69ed61d4bd1ebb9c11db2b818f3848d
Philasande Bhani <pbhanina@gmail.com>
2023-07-25 14:20:02 +0200

    pushing routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/components/home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,children:[
+      {
+        path:'header',component: HeaderComponent
+      },
+      {
+        path: 'footer',
+        component: FooterComponent,
+
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ComponentsComponent } from './shared/components/components.component';
+import { HomeComponent } from './shared/components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    ComponentsComponent
+    ComponentsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

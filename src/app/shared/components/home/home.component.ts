5dec61a29faafa4b529265cd7e764e1f8121b827
Philasande Bhani <pbhanina@gmail.com>
2023-07-26 14:50:09 +0200

    Commiting functioning home page

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+form: FormGroup= new FormGroup({})
+  constructor(private formBuilder:FormBuilder){
+  }
+  formBinding(){
+    this.form = this.formBuilder.group({
+      amount:['']
+    });
+  }
+  ngOnInit(): void {
+    this.formBinding();
+  }
   
-
+submit(){
+alert(this.form.value.amount)
+}
 }
